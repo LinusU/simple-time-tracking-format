688c7401099b7b90f0c082bb1087c576eb51e32a
Linus Unnebäck <linus@folkdatorn.se>
2019-11-21 14:27:16 +0000

    🐛 Fix handling of time ending at 24:00

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const getStdin = require('get-stdin')
-const differenceInMinutes = require('date-fns/difference_in_minutes')
+const differenceInMinutes = require('date-fns/differenceInMinutes')
 
 const { parse } = require('.')
 
@@ -10,9 +10,15 @@ getStdin().then((input) => {
   const months = [...new Set(entries.map(e => e.start.slice(0, 7)))]
 
   for (const month of months) {
-    const minutes = entries.filter(e => e.start.startsWith(month)).reduce((mem, e) => mem + differenceInMinutes(e.end, e.start), 0)
-    const hours = Math.floor(minutes / 60)
+    const minutes = entries.filter(e => e.start.startsWith(month)).reduce((mem, e) => {
+      const diff = differenceInMinutes(new Date(e.end), new Date(e.start))
 
-    console.log(`${month} - ${hours}:${String(minutes % 60).padStart(2, '0')}`)
+      if (diff <= 0) throw new Error('Start time was after or at end time')
+      if (diff > 1440) throw new Error('Failed to calculate duration')
+
+      return mem + diff
+    }, 0)
+
+    console.log(`${month} - ${Math.floor(minutes / 60)}:${String(minutes % 60).padStart(2, '0')}`)
   }
 })
